abb1dccc1696e0f9241ddfa1a8f4142f661f3583
Alonso L <me@on247.me>
2020-12-01 22:32:53 -0600

    Configurar hive para almacenar objetivos

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:entregable2/login/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await Hive.initFlutter();
+  await Hive.openBox('goals');
   runApp(MyApp());
 }
 
